51265101cac0acc11ce71ef39a9ecf68cffe418f
Mail <mailovmr@gmail.com>
2021-12-27 04:19:12 +0300

    main

--- cmake-build-debug/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mailo/CLionProjects/lab_2/lib
-# Build directory: C:/Users/mailo/CLionProjects/lab_2/cmake-build-debug/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
